7cf208d27260f6681427f728a9d05bcc9c24233e
MaKarf <51786690+MaKarf@users.noreply.github.com>
2024-08-31 03:33:44 +0000

    Cleaned icons

--- lib/pyrevitExtensionManager.py
+++ lib/pyrevitExtensionManager.py
@@ -113,5 +113,5 @@ class PyRevitExtensionManager:
 
 if __name__ == "__main__":
     mk = PyRevitExtensionManager()
+    mk.update_pyrevit_extensions_json(OperationType.remove_from_json)
     mk.update_pyrevit_extensions_json(OperationType.add_to_json)
-    # mk.update_pyrevit_extensions_json(OperationType.remove_from_json)
